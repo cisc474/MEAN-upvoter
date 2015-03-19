3e16db7c34931bea0d881f5b2fbef0c85bf0c0c2
AndyNovo <andy@novocin.com>
2015-03-18 23:13:03 -0400

    comments in the angular code

--- client/app.js
+++ client/app.js
@@ -1,3 +1,4 @@
+//we'll use routes for this one, our app is "upvoter"
 var app = angular
   .module("upvoter", ['ngRoute']);
 
@@ -9,6 +10,7 @@ app .config([
         templateUrl: "home.html",
         controller : "homeController",
         resolve : {
+          //This resolve thing is new here, it triggers some functions before displaying the screen.
           postPromise: ["posts", function(posts){
             return posts.fetchAll();}]
         }
@@ -18,6 +20,7 @@ app .config([
         controller : "topicController",
         resolve : {
           post: ["$route", "posts", function($route, posts) {
+            //I had to consolt stack overflow for the "current" params bit.
             console.log($route.current.params);
             return posts.fetchOne($route.current.params.id);
           }]
@@ -29,11 +32,14 @@ app .config([
   }
 ]);
 
+//this factory is an angular way of creating a "singleton" (look it up under design patterns)
+// we will create an object here that can be passed between controllers using the dependency injection (the square brackets everywhere)
+//the name will be "posts" and it will handle the AJAX too.
 app.factory('posts', ["$http", function($http){
 
   var object = {
     posts : [],
-    
+    //Now we call the API as it was laid out in the server file.
     fetchAll : function(){
         return $http.get("/posts").success(function(data){
             angular.copy(data, object.posts);
@@ -73,8 +79,10 @@ app.factory('posts', ["$http", function($http){
 
 }]);
 
+
 app.controller(
   "topicController", 
+  //post is the results of the resolve action when hitting a specific route, it is thus also the result of "fetchOne"
   ["$scope", "posts", "post", 
     function($scope, posts, post){
       $scope.text = "";
@@ -83,14 +91,18 @@ app.controller(
       $scope.comments = post.comments;
 
       $scope.addComment = function(){
+        //this prevents adding an empty comment
         if (!$scope.text || $scope.text === ''){ return; }
+        //calling the factory method, notice that we are saving the "success" callback to here 
+        //so we can inject the comments into scope
         posts.addComment(post._id, {text: $scope.text, upvotes: 0})
           .success(function(comment) {
             $scope.comments.push(comment);
         });
+        //clear the input
         $scope.text = '';
       }
-
+//something to call onclicks
       $scope.increaseCommentUpvotes = function(comment){
         posts.upvoteComment(post, comment);
       }
@@ -104,7 +116,7 @@ app.controller(
     function($scope, posts){
       $scope.text = "";
       $scope.posts = posts.posts; 
-
+//very similar to above code.
       $scope.addPost = function(){
         if (!$scope.text || $scope.text === ''){ return; }
         posts.createPost({text: $scope.text, upvotes: 0, comments: []});
