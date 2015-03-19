88b586a8ee86eaa1e54c60afc3b9dd7c7d94b492
AndyNovo <andy@novocin.com>
2015-03-18 23:05:17 -0400

    comments in server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var path = require('path');
 var express = require('express');
 var router = express();
 var server = http.createServer(router);
+
+//mongoose is the node/mongodb connection layer
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 //Insecure by the way
@@ -10,19 +12,22 @@ router.use( express.bodyParser() );
 
 router.use(express.static(path.resolve(__dirname, 'client')));
 
+//this will be different for every different mongo install, I was using webfaction and mongodb listens on its own port
 mongoose.connect("mongodb://127.0.0.1:26329/test");
 
+//This is the mongoose/mongo version of a table declaration
 var PostSchema = new mongoose.Schema({ 
   text: String,
   upvotes: {type: Number, default: 0},
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
-
+//this is the upvoting method
 PostSchema.methods.upvote = function(cb) {
   this.upvotes += 1;
   this.save(cb);
 };
 
+//this links the schema and Posts
 mongoose.model('Post', PostSchema);
 
 var CommentSchema = new mongoose.Schema({
@@ -38,8 +43,22 @@ CommentSchema.methods.upvote = function(cb) {
 
 mongoose.model('Comment', CommentSchema);
 
+//This declares my classes for this server/app
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+
+/*
+this is where I create the API part I want the following API:
+
+GET /posts (fetch all posts, low detail)
+GET /posts/:postid (fetch all of the data for one post (all comments))
+
+POST /posts (create a new post using the posted data and return the new post data including the freshly created _id)
+POST /posts/:postid/comments (create a new comment using the posted data and return the new comment including _id)
+
+PUT /posts/:postid/upvote (upvotes the target post)
+PUT /posts/:postid/comments/:commentid/upvotes (upvotes the target comment)
+*/
 
 router.get("/posts", function(req, res, next){
   Post.find(function(err, posts){
@@ -58,6 +77,12 @@ router.post('/posts', function(req, res, next) {
   });
 
 });
+
+/*
+This is a slick trick I learned for this project, 
+when a :post variable inside of a route this function will get called 
+BEFORE the route.  Allows some preprocessing.
+*/
 
 router.param('post', function(req, res, next, id) {
   var query = Post.findById(id);
@@ -85,6 +110,7 @@ router.param('comment', function(req, res, next, id) {
 });
 
 router.get('/posts/:post', function(req, res, next) {
+  //this populate comes from mongoose, it loads up the comments associated with the post
   req.post.populate('comments', function(err, post) {
     if (err) { return next(err); }
 
@@ -125,6 +151,8 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
   });
 });
 
+
+//these ports are for my webfaction host, adjust accordingly
 server.listen(32013 || 3000, "0.0.0.0", function(){
   var addr = server.address();
   console.log("Upvote server listening at", addr.address + ":" + addr.port);
